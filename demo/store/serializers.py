aa2c2466fdbee9137967bdb1e6d21bcb57dd9067
Liberbell <liberbell@gmail.com>
2019-11-06 07:53:46 +0900

    serializers6

--- demo/store/serializers.py
+++ demo/store/serializers.py
@@ -11,9 +11,3 @@ class ProductSerializer(serializers.ModelSerializer):
         data['is_on_sale'] = instance.is_on_sale()
         date['current_price'] = instance.current_price()
         return data
-
-
-from store.models import Product
-product = Product.object.all()[0]
-product = Product.objects.all()[0]
-from store.serializers import ProductSerializer
