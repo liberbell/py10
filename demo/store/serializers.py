309f4a0d6dd0c14565ec340e0986147d2f73ce9e
Liberbell <liberbell@gmail.com>
2019-11-06 07:52:45 +0900

    serializers6

--- demo/store/serializers.py
+++ demo/store/serializers.py
@@ -1,4 +1,4 @@
-from django_framework import serializers
+from rest_framework import serializers
 from store.models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -11,3 +11,9 @@ class ProductSerializer(serializers.ModelSerializer):
         data['is_on_sale'] = instance.is_on_sale()
         date['current_price'] = instance.current_price()
         return data
+
+
+from store.models import Product
+product = Product.object.all()[0]
+product = Product.objects.all()[0]
+from store.serializers import ProductSerializer
