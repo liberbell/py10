a9481aeb6aec52ee96990c7dfe9879579bbb7dcf
Liberbell <liberbell@gmail.com>
2019-11-06 10:05:26 +0900

    api_views4

--- demo/store/api_views.py
+++ demo/store/api_views.py
@@ -3,4 +3,5 @@ from store.serializers import ProductSerializer
 from store.models import Product
 
 class ProductList(ListAPIView):
-    queryset = Product.objects.all() 
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
