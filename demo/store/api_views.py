514c5d458341e3689fa5e7f3e359c5782c8775f5
Liberbell <liberbell@gmail.com>
2019-11-07 07:41:54 +0900

    api_views1

--- demo/store/api_views.py
+++ demo/store/api_views.py
@@ -1,4 +1,6 @@
 from rest_framework.generics import ListAPIView
+from django_filters.rest_framework import DjangoFilterBackend
+
 from store.serializers import ProductSerializer
 from store.models import Product
 
