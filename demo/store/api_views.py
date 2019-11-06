5d374ab178c372f9e6c456c4d280fd0e63809dab
Liberbell <liberbell@gmail.com>
2019-11-06 10:04:07 +0900

    api_views3

--- demo/store/api_views.py
+++ demo/store/api_views.py
@@ -1,3 +1,6 @@
 from rest_framework.generics import ListAPIView
 from store.serializers import ProductSerializer
 from store.models import Product
+
+class ProductList(ListAPIView):
+    queryset = Product.objects.all() 
