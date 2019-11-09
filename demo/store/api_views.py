ce8ee2e86ebff144f68af1fd56429c36c8f7c549
Liberbell <liberbell@gmail.com>
2019-11-09 13:10:49 +0900

    api_views8

--- demo/store/api_views.py
+++ demo/store/api_views.py
@@ -1,5 +1,5 @@
 from rest_framework.exceptions import ValidationError
-from rest_framework.generics import ListAPIView, CreateAPIView, DestroyAPIView
+from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveUpdateDestroy
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
 from rest_framework.pagination import LimitOffsetPagination
