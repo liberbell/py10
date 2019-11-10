e80055f17c54c942a8cb6e735639eedb722cff97
Liberbell <liberbell@gmail.com>
2019-11-11 07:41:22 +0900

    serializer5

--- demo/store/serializers.py
+++ demo/store/serializers.py
@@ -12,6 +12,7 @@ class ProductSerializer(serializers.ModelSerializer):
     current_price = serializers.FloatField(read_only=True)
     description = serializers.CharField(min_length=2, max_length=200)
     cart_items = serializers.SerializerMethodField()
+    price = serializers.FloatField(min_value=1.00, max_value=100000)
 
     class Meta:
         model = Product
