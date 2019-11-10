bf9cedd33a5476e1e67ca6c39d4db9cfb6f8832d
Liberbell <liberbell@gmail.com>
2019-11-10 14:00:07 +0900

    serializer2

--- demo/store/serializers.py
+++ demo/store/serializers.py
@@ -3,6 +3,7 @@ from store.models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
     is_on_sale = serializers.BooleanField(read_only=True)
+    current_price = serializers.FloatField(read_only=True)
     class Meta:
         model = Product
         fields = ('id', 'name', 'description', 'price', 'sale_start', 'sale_end')
