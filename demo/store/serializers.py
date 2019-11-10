9c4847f70d9f62bdc665e20018756c27de01d70d
Liberbell <liberbell@gmail.com>
2019-11-11 07:38:22 +0900

    serializer3

--- demo/store/serializers.py
+++ demo/store/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from store.models import Product
+from store.models import Product, ShoppingCartItem
 
 class CartItemSerializer(serializers.ModelSerializer):
     class Meta:
