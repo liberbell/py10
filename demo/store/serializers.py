75857c96b411b2c9f69c27548d983383c6495e02
Liberbell <liberbell@gmail.com>
2019-11-10 14:07:24 +0900

    serializer4

--- demo/store/serializers.py
+++ demo/store/serializers.py
@@ -1,11 +1,16 @@
 from rest_framework import serializers
 from store.models import Product
 
+class CartItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ShoppingCartItem
+        fields = ('product', )
+
 class ProductSerializer(serializers.ModelSerializer):
     is_on_sale = serializers.BooleanField(read_only=True)
     current_price = serializers.FloatField(read_only=True)
     description = serializers.CharField(min_length=2, max_length=200)
-    
+
     class Meta:
         model = Product
         fields = ('id', 'name', 'description', 'price', 'sale_start', 'sale_end',
