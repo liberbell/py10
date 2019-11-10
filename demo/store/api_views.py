cacf910912aaeb11197850c911480bd2757483ee
Liberbell <liberbell@gmail.com>
2019-11-10 11:37:33 +0900

    api_views1

--- demo/store/api_views.py
+++ demo/store/api_views.py
@@ -45,7 +45,19 @@ class ProductCreate(CreateAPIView):
             raise ValidationError({ 'price': 'A Valid number is required' })
         return super().create(request, *args, **kwargs)
 
-class ProductDestroy(DestroyAPIView):
+# class ProductDestroy(DestroyAPIView):
+#     queryset = Product.objects.all()
+#     lookup_field = 'id'
+#
+#     def delete(self, request, *args, **kwargs):
+#         product_id = request.data.get('id')
+#         response = super().delete(request, *args, **kwargs)
+#         if response.status_code == 204:
+#             from django.core.cache import cache
+#             cache.delete('product_data_{}'.format(product_id))
+#         return response
+
+class ProductRetriveUpdateDestroy(RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     lookup_field = 'id'
 
