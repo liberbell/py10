c380a39aa5f3c71cec72d2e36d110bdfebb672c6
Liberbell <liberbell@gmail.com>
2019-11-10 13:59:06 +0900

    serializer1

--- demo/store/serializers.py
+++ demo/store/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from store.models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
+    is_on_sale = serializers.BooleanField(read_only=True)
     class Meta:
         model = Product
         fields = ('id', 'name', 'description', 'price', 'sale_start', 'sale_end')
