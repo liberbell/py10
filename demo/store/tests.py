db37254dc42135fc98f52d816b7d25f73bb339e8
Liberbell <liberbell@gmail.com>
2019-11-13 07:54:06 +0900

    tests3

--- demo/store/tests.py
+++ demo/store/tests.py
@@ -1,4 +1,8 @@
 from django.test import TestCase
 from rest_framework.test import APITestCase
+from store.models import Product
 
 # Create your tests here.
+class ProductCreateTestCase(APITestCase):
+    def test_create_product(self):
+        initial_product_count
