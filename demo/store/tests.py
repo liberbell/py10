efa7cada0e1b8f340757ca41b175e77658152bfe
Liberbell <liberbell@gmail.com>
2019-11-13 07:56:28 +0900

    tests4

--- demo/store/tests.py
+++ demo/store/tests.py
@@ -5,4 +5,9 @@ from store.models import Product
 # Create your tests here.
 class ProductCreateTestCase(APITestCase):
     def test_create_product(self):
-        initial_product_count
+        initial_product_count = Product.objects.count()
+        product_attrs = {
+            'name': 'New Product',
+            'description': 'Awesome Product',
+            'price': '123,45',
+        }
