6752db2a0893254d111fa3f5b38c61fa918e2ed6
Liberbell <liberbell@gmail.com>
2019-11-15 18:20:51 +0900

    tests7

--- demo/store/tests.py
+++ demo/store/tests.py
@@ -24,19 +24,19 @@ class ProductCreateTestCase(APITestCase):
             float(product_attrs['price']),
         )
 
-class ProductDestroyTestCase(APITestCase):
-    def test_delete_product(self):
-        initial_product_count = Product.objects.count()
-        product_id = Product.objects.first().id
-        self.client.delete('/api/v1/products/{}/'.format(product_id))
-        self.assertEqual(
-                        Product.objects.count(),
-                        initial_product_count - 1,
-                        )
-        self.assertRaises(
-                        Product.DoesNotExist,
-                        Product.objects.get, id=product_id,
-                        )
+# class ProductDestroyTestCase(APITestCase):
+#     def test_delete_product(self):
+#         initial_product_count = Product.objects.count()
+#         product_id = Product.objects.first().id
+#         self.client.delete('/api/v1/products/{}/'.format(product_id))
+#         self.assertEqual(
+#                         Product.objects.count(),
+#                         initial_product_count - 1,
+#                         )
+#         self.assertRaises(
+#                         Product.DoesNotExist,
+#                         Product.objects.get, id=product_id,
+#                         )
 
 class ProductListTestCase(APITestCase):
     def test_list_products(self):
